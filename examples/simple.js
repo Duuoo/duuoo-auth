ff95c27270c3a9fc691583a07acab99f6ef381f9
fnky <chrs@cbp.io>
2017-06-14 15:04:39 +0200

    Update examples

---
+++ examples/simple.js
@@ -0,0 +1,53 @@
+const express = require('express')
+// const session = require('express-session')
+const authentication = require('../lib')
+// const RedisStore = require('connect-redis')(session)
+// const sessionStore = new RedisStore({ host: 'localhost' })
+
+const app = express()
+
+const auth = authentication({
+  strategies: {
+    auth0: {
+      verify (accessToken, refreshToken, extraParams, profile, done) {
+        done(null, profile)
+      },
+      options: {
+        domain: 'abc.com',
+        clientID: 'leCLientId',
+        clientSecret: 'leClientSecret',
+        callbackURL: 'leCallbackUrl',
+      },
+    },
+  },
+})
+
+// Required in order for Passport to serialize user to a session.
+auth.serializeUser((user, done) => {
+  done(null, user)
+})
+
+auth.deserializeUser((user, done) => {
+  done(null, user)
+})
+
+app.use(auth.initialize())
+app.use(auth.session())
+
+function ensureLoggedIn (req, res, next) {
+  if (req.isAuthenticated && !req.isAuthenticated()) {
+    res.redirect('/')
+  }
+}
+
+app.get('/auth0/callback', auth.authenticate('auth0', { failureRedirect: '/' }), (req, res) => {
+  res.redirect(req.session.redirectTo || '/welcome')
+})
+
+app.get('/welcome', ensureLoggedIn, (req, res) => {
+  res.statusCode(200).send(`Welcome, ${req.user.displayName}!`)
+})
+
+app.listen(1337, () => {
+  console.log('Listening at localhost:1337')
+})
